cd6eeef77876fd4689010b702b6b26295a8e41e5
Shean <csheanjohnson@gmail.com>
2020-08-10 18:54:15 -0400

    transferring project from enterprise account

---
+++ app.js
@@ -0,0 +1,452 @@
+let config = {
+  headers: {
+    "X-API-Key": "s2BrtezFJBxLW0uBwUGekk8X8p7sIKX0zS5jdHKY"
+  }
+}
+let img = [
+  {
+    state: "ALASKA",
+    outline: "./img/AK.png",
+    governor: "Mike Dunleavy"
+  },
+  {
+    state: "ALABAMA",
+    outline: "./img/AL.png",
+    governor: "Kay Ivey"
+  },
+  {
+    state: "ARKANSAS",
+    outline:  "./img/AR.png",
+    governor: "Asa Hutchinson"
+  },
+  {
+    state: "ARIZONA",
+    outline:  "./img/AZ.png",
+    governor: "Doug Ducey"
+  },
+  {
+    state: "CALIFORNIA",
+    outline:  "./img/CA.png",
+    governor: "Gavin Newsom"
+  },
+  {
+    state: "COLORADO",
+    outline:  "./img/CO.png",
+    governor: "Jared Polis"
+  },
+  {
+    state: "CONNECTICUT",
+    outline:  "./img/CT.png",
+    governor: "Ned Lamont"
+  },
+  {
+    state: "DISTRICT OF COLUMBIA",
+    outline:  "./img/DC.png",
+    governor: "Muriel Bowser"
+  },
+  {
+    state: "DELEWARE",
+    outline:  "./img/DE.png",
+    governor: "John Carney"
+  },
+  {
+    state: "FLORIDA",
+    outline:  "./img/FL.png",
+    governor: "Ron DeSantis"
+  },
+  {
+    state: "GEORGIA",
+    outline:  "./img/GA.png",
+    governor: "Brian Kemp"
+  },
+  {
+    state: "HAWAII",
+    outline:  "./img/HI.png",
+    governor: "David Ige"
+  },
+  {
+    state: "IOWA",
+    outline:  "./img/IA.png",
+    governor: "Kim Reynolds"
+  },
+  {
+    state: "IDAHO",
+    outline:  "./img/ID.png",
+    governor: "Brad Little"
+  },
+  {
+    state: "ILLINOIS",
+    outline:  "./img/IL.png",
+    governor: "J. B. Pritzker"
+  },
+  {
+    state: "INDIANA",
+    outline: "./img/IN.png",
+    governor: "Eric Holcomb"
+  },
+  {
+    state: "KANSAS",
+    outline:  "./img/KS.png",
+    governor: "Laura Kelly"
+  },
+  {
+    state: "KENTUCKY",
+    outline:  "./img/KY.png",
+    governor: "Andy Beshear"
+  },
+  {
+    state: "LOUISIANA",
+    outline:  "./img/LA.png",
+    governor: "John Bel Edwards"
+  },
+  {
+    state: "MASSACHUSETTS",
+    outline:  "./img/MA.png",
+    governor: "Charlie Baker"
+  },
+  {
+    state: "MARYLAND",
+    outline:  "./img/MD.png",
+    governor: "Larry Hogan"
+  },
+  {
+    state: "MAINE",
+    outline:  "./img/ME.png",
+    governor: "Janet Mills"
+  },
+  {
+    state: "MICHIGAN",
+    outline:  "./img/MI.png",
+    governor: "Gretchen Whitmer"
+  },
+  {
+    state: "MINNESOTA",
+    outline:  "./img/MN.png",
+    governor: "Tim Walz"
+  },
+  {
+    state: "MISOURI",
+    outline:  "./img/MO.png",
+    governor: "Mike Parson"
+  },
+  {
+    state: "MISSISSIPPI",
+    outline: "./img/MS.png",
+    governor: "Tate Reeves"
+  },
+  {
+    state: "MONTANA",
+    outline:  "./img/MT.png",
+    governor: "Steve Bullock"
+  },
+  {
+    state: "NORTH CAROLINA",
+    outline:  "./img/NC.png",
+    governor: "Roy Cooper"
+  },
+  {
+    state: "NORTH DAKOTA",
+    outline:  "./img/ND.png",
+    governor: "Doug Burgum"
+  },
+  {
+    state: "NEBRASKA",
+    outline:  "./img/NE.png",
+    governor: "Pete Ricketts"
+  },
+  {
+    state: "NEW HAMPSHIRE",
+    outline:  "./img/NH.png",
+    governor: "Chris Sununu"
+  },
+  {
+    state: "NEW JERSEY",
+    outline:  "./img/NJ.png",
+    governor: "Phil Murphy"
+  },
+  {
+    state: "NEW MEXICO",
+    outline:  "./img/NM.png",
+    governor: "Michelle Lujan Grisham"
+  },
+  {
+    state: "NEVADA",
+    outline:  "./img/NV.png",
+    governor: "Steve Sisolak"
+  },
+  {
+    state: "NEW YORK",
+    outline:  "./img/NY.png",
+    governor: "Andrew Cuomo"
+  },
+  {
+    state: "OHIO",
+    outline:  "./img/OH.png",
+    governor: "Mike DeWine"
+  },
+  {
+    state: "OKLAHOMA",
+    outline:  "./img/OK.png",
+    governor: "Kevin Stitt"
+  },
+  {
+    state: "OREGON",
+    outline:  "./img/OR.png",
+    governor: "Kate Brown"
+  },
+  {
+    state: "PENNSYLVANIA",
+    outline:  "./img/PA.png",
+    governor: "Tom Wolf"
+  },
+  {
+    state: "RHODE ISLAND",
+    outline:  "./img/RI.png",
+    governor: "Gina Raimondo"
+  },
+  {
+    state: "SOUTH CAROLINA",
+    outline:  "./img/SC.png",
+    governor: "Henry McMaster"
+  },
+  {
+    state: "SOUTH DAKOTA",
+    outline:  "./img/SD.png",
+    governor: "Kristi Noem"
+  },
+  {
+    state: "TENNESSEE",
+    outline:  "./img/TN.png",
+    governor: "Bill Lee"
+  },
+  {
+    state: "TEXAS",
+    outline:  "./img/TX.png",
+    governor: "Greg Abbott"
+  },
+  {
+    state: "UTAH",
+    outline:  "./img/UT.png",
+    governor: "Gary Herbert"
+  },
+  {
+    state: "VIRGINIA",
+    outline:  "./img/VA.png",
+    governor: "Ralph Northam"
+  },
+  {
+    state: "VERMONT",
+    outline:  "./img/VT.png",
+    governor: "Phil Scott"
+  },
+  {
+    state: "WASHINGTON",
+    outline:  "./img/WA.png",
+    governor: "Jay Inslee"
+  },
+  {
+    state: "WISCONSIN",
+    outline:  "./img/WI.png",
+    governor: "Tony Evers"
+  },
+  {
+    state: "WEST VIRGINIA",
+    outline:  "./img/WV.png",
+    governor: "Jim Justice"
+  },
+  {
+    state: "WYOMING",
+    outline:  "./img/WY.png",
+    governor: "Mark Gordon"
+  },
+  {
+    state: "PUERTO RICO",
+    outline:  "./img/PR.png",
+    governor: "Wanda Vazquez Garced"
+  },
+  {
+    state: "AMERICAN SAMOA",
+    outline:  "./img/AS.png",
+    governor: "Lolo Matalasi Moliga"
+  },
+  {
+    state: "GUAM",
+    outline:  "./img/GU.png",
+    governor: "Lou Leon Guerrero"
+  },
+  {
+    state: "MARIANA ISLANDS",
+    outline:  "./img/MP.png",
+    governor: "Ralph Torres"
+  },
+  {
+    state: "VIRGIN ISLANDS",
+    outline:  "./img/VI.png",
+    governor: "Albert Bryan"
+  }
+]
+let stateList;
+let option;
+let senResponse;
+let totalStats;
+const search = document.querySelector('.state-list');
+const select = document.querySelector('#select-state')
+async function dropDown() {
+  const url = "https://covidtracking.com/api/v1/states/current.json"
+  try {
+    const response = await axios.get(url)
+    stateList = response.data
+    stateList.forEach( (obj, index) => {
+      const option = document.createElement('option')
+      option.value = index
+      option.className = obj.state
+      option.text = `${img[index].state}`
+      select.append(option)
+    })
+  } catch (error) {
+    alert(`Err. ${error}`)
+  }
+}
+dropDown()
+search.addEventListener(`change`, (e) => {
+  e.preventDefault();
+  const index = select.options[select.selectedIndex].value
+  display(index)
+  displayData()
+});
+function displayData() {
+  let displayDiv = document.querySelector('.data-display');
+    displayDiv.style.display = "block";
+  
+}
+async function display(index) {
+  const state = select.options[select.selectedIndex].className
+  const urlSen = `https://api.propublica.org/congress/v1/members/senate/${state}/current.json`
+  try {
+    const response = await axios.get(urlSen, config)
+    senResponse = response.data
+    console.log(senResponse)
+  } catch (error) {
+    console.log(error)
+  }
+  let displayArea = document.querySelector('.data-display')
+  displayArea.innerHTML = 
+  `
+  <div class="dataImg" style='background-image: url("${img[index].outline}");'>
+    <div class="stateDataDisplayDiv">
+    <h1 id="state-name">${img[index].state}</h2>
+    <h2 id="positiveCases">Total Positive Cases: <span>${numComma(stateList[index].positive)}</span></h2>
+    <h2 id="mortality">Current Mortalities: <span>${numComma(stateList[index].death)}</span></h2>
+    </div>
+    <div class="stateReps">
+    <h2>State Representatives:</h2>
+    <h5>Governor: ${img[index].governor}</h5>
+    <h5>Senator: ${senResponse.results[0].name} <a target="_blank" href="https://twitter.com/${senResponse.results[0].twitter_id}/">    @${senResponse.results[0].twitter_id}</a></h5>
+    <h5>Senator: ${senResponse.results[1].name}<a target="_blank" href="https://twitter.com/${senResponse.results[1].twitter_id}/">    @${senResponse.results[1].twitter_id}</a></h5>
+    </div>
+    </div> 
+  `
+}
+async function newsDisplay() {
+  let newsUrl = `https://newsapi.org/v2/everything?q=covid&apiKey=f5abcb1acb1942188a4163e71207fab5`
+    let news = await axios.get(newsUrl)
+    const articlesArray = news.data.articles
+  let newsScroll = document.querySelector('.news-display')
+  newsScroll.innerHTML =
+    `
+    <div class="article">
+    <a target="_blank" href="${articlesArray[0].url}">
+      <img class="articleImg" src='${articlesArray[0].urlToImage}'>
+      <h4>${articlesArray[0].title}</h4>
+      <h6>${articlesArray[0].author}</h6></a>
+    </div>
+    <div class="article">
+    <a target="_blank" href="${articlesArray[1].url}">
+      <img class="articleImg" src='${articlesArray[1].urlToImage}'>
+      <h4>${articlesArray[1].title}</h4>
+      <h6>${articlesArray[1].author}</h6></a>
+    </div>
+    <div class="article">
+    <a target="_blank" href="${articlesArray[2].url}">
+      <img class="articleImg" src='${articlesArray[2].urlToImage}'>
+      <h4>${articlesArray[2].title}</h4>
+      <h6>${articlesArray[2].author}</h6></a>
+    </div>
+    <div class="article">
+    <a target="_blank" href="${articlesArray[3].url}">
+      <img class="articleImg" src='${articlesArray[3].urlToImage}'>
+      <h4>${articlesArray[3].title}</h4>
+      <h6>${articlesArray[3].author}</h6></a>
+    </div>
+    <div class="article">
+    <a target="_blank" href="${articlesArray[4].url}">
+      <img class="articleImg" src='${articlesArray[4].urlToImage}'>
+      <h4>${articlesArray[4].title}</h4>
+      <h6>${articlesArray[4].author}</h6></a>
+    </div>
+    <div class="article">
+    <a target="_blank" href="${articlesArray[5].url}">
+      <img class="articleImg" src='${articlesArray[5].urlToImage}'>
+      <h4>${articlesArray[5].title}</h4>
+      <h6>${articlesArray[5].author}</h6></a>
+    </div>
+    <div class="article">
+    <a target="_blank" href="${articlesArray[6].url}">
+      <img class="articleImg" src='${articlesArray[6].urlToImage}'>
+      <h4>${articlesArray[6].title}</h4>
+      <h6>${articlesArray[6].author}</h6></a>
+    </div>
+    <div class="article">
+    <a target="_blank" href="${articlesArray[7].url}">
+      <img class="articleImg" src='${articlesArray[7].urlToImage}'>
+      <h4>${articlesArray[7].title}</h4>
+      <h6>${articlesArray[7].author}</h6></a>
+    </div>
+    <div class="article">
+    <a target="_blank" href="${articlesArray[8].url}">
+      <img class="articleImg" src='${articlesArray[8].urlToImage}'>
+      <h4>${articlesArray[8].title}</h4>
+      <h6>${articlesArray[8].author}</h6></a>
+    </div>
+    <div class="article">
+    <a target="_blank" href="${articlesArray[9].url}">
+      <img class="articleImg" src='${articlesArray[9].urlToImage}'>
+      <h4>${articlesArray[9].title}</h4>
+      <h6>${articlesArray[9].author}</h6></a>
+    </div>
+    
+    `
+  }
+newsDisplay()
+async function fetchData() {
+  let url = 'https://covidtracking.com/api/v1/us/current.json'
+  try {
+    let response = await axios.get(url)
+    const data = response.data[0]
+    banner(data)
+  } catch (error) {
+    alert(`${error}`)
+  }
+}
+fetchData()
+function banner(data) {
+  let bannerInfo = 
+    `
+    <h2>Total U.S. Cases: <span>${numComma(data.positive)}</span></h2>
+    <h2>Total U.S. Mortality: <span>${numComma(data.death)}</span></h2>
+    `
+  document.querySelector('#banner').insertAdjacentHTML('beforeend', bannerInfo)
+}
+function numComma(num) {
+  num = num.toString();
+  var pattern = /(-?\d+)(\d{3})/;
+  while (pattern.test(num))
+      num = num.replace(pattern, "$1,$2");
+  return num;
+}
+
+
+
+
+
+
