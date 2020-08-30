d399cea83ca2f00a1d6e35f8086348d1076eb8b4
Shean Johnson <csheanjohnson@gmail.com>
2020-08-30 19:34:18 -0400

    CORSanywhere addition for deploy until better solution

--- client/src/components/News/News.js
+++ client/src/components/News/News.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const newsDisplay = async () => {
-    const newsUrl = `https://newsapi.org/v2/everything?q=covid&apiKey=f5abcb1acb1942188a4163e71207fab5`
+    const newsUrl = `https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/everything?q=covid&apiKey=f5abcb1acb1942188a4163e71207fab5`;
     try {
         const response = await axios.get(newsUrl)
         let nationalNews = response.data.articles
--- client/src/components/StateData/StateData.jsx
+++ client/src/components/StateData/StateData.jsx
@@ -1,21 +1,22 @@
-import React, { Component } from 'react'
-import './StateData.css'
-
+import React, { Component } from "react";
+import "./StateData.css";
+import { stateData } from "./StateData.js";
 
 export default class StateData extends Component {
-    render() {
-        return (
-          <div className='component stateData'>
-            <h4>View Data by State</h4>
-            <select name='State Data' id=''>
-              <option>Alabama</option>
-              <option>Alaska</option>
-              <option>Arizona</option>
-              <option>Arkansas</option>
-              <option>California</option>
-              <option>Colorado</option>
-            </select>
-          </div>
-        );
-    }
+  async componentDidMount() {
+    let selections = await stateData();
+    this.setState({ selections: selections});
+    console.log(this.state.selections);
+  }
+  state = {
+      selections: '',
+  };
+  render() {
+    return (
+      <div className='component stateData'>
+        <h4>View Data by State</h4>
+        <select></select>
+      </div>
+    );
+  }
 }
