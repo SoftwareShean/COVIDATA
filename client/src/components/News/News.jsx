042ea3970d72b5454fd9f278d9b0d0dce4013a4d
Shean Johnson <csheanjohnson@gmail.com>
2020-09-23 19:46:49 -0400

    refactored News async API call to NYT API instead of NEWSAPI which doesnt allow for calls outside of LocalHost.

--- client/src/components/News/News.jsx
+++ client/src/components/News/News.jsx
@@ -4,9 +4,15 @@ import './News.css'
 import {newsDisplay} from './News.js'
 
 export default class News extends Component {
-    async componentDidMount() {
-        let topStories = await newsDisplay();
-        this.setState({ topStories: topStories })
+  state = {
+    articles: ''
+  }
+  
+  async componentDidMount() {
+    let topStories = await newsDisplay();
+    console.log(topStories)
+    this.setState({ articles: topStories })
+    console.log(this.state.articles)
     }
 
     state = {
@@ -14,13 +20,13 @@ export default class News extends Component {
     }
 
     render() {
-      const {topStories} = this.state
+      const {articles} = this.state
     return (
       <div className='news component'>
         <h1>Top News</h1>
         <div className='carosel'>
-                {topStories &&
-                    topStories.map((article) => (<ArticleCard data={article} key={article.title}/>))
+                {articles &&
+                    articles.map((article) => (<ArticleCard data={article} key={article.title}/>))
                 }
         </div>
       </div>
