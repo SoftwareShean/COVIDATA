8e410401cea3770f434be1f1e3460971ffba5361
Shean Johnson <csheanjohnson@gmail.com>
2020-09-23 18:44:44 -0400

    updates to RepSearch and StateData components

--- client/src/components/StateData/StateData.js
+++ client/src/components/StateData/StateData.js
@@ -1,11 +1,11 @@
-import axios from 'axios'
+import axios from "axios";
 
 export const stateData = async () => {
   const stateUrl = `https://api.covidtracking.com/v1/states/current.json`;
   try {
-    const response = await axios.get(stateUrl)
-      let stateList = response.data
-      console.log(stateList)
+    const response = await axios.get(stateUrl);
+    let stateList = response.data;
+    console.log(stateList);
     // stateList.forEach( (obj, index) => {
     //   const option = document.createElement('option')
     //   option.value = index
@@ -14,6 +14,6 @@ export const stateData = async () => {
     //   select.append(option)
     // })
   } catch (error) {
-    alert(`Err. ${error}`)
+    alert(`Err. ${error}`);
   }
-}
+};
